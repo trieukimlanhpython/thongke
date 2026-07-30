a17583e5b8ecace467a0f5c4e74a312bbe74c3d9
trieukimlanhpython <trieukimlanh.python@gmail.com>
2026-07-30 10:14:49 +0700

    Update thongke.py

--- thongke.py
+++ thongke.py
@@ -82,19 +82,19 @@ def read_gsheet(link):
 # ==========================================================
 links = {
     "df1": (
-        "https://docs.google.com/spreadsheets/d/1F_w2yXvD66m0DeSmUrn-mFYcHwr2VKL6JYS6-bdATtQ/export?format=csv&gid=2080729380"
+        "https://docs.google.com/spreadsheets/d/1F_w2yXvD66m0DeSmUrn-mFYcHwr2VKL6JYS6-bdATtQ/edit?gid=2080729380#gid=2080729380"
     ),
     "df2": (
-        "https://docs.google.com/spreadsheets/d/1F_w2yXvD66m0DeSmUrn-mFYcHwr2VKL6JYS6-bdATtQ/export?format=csv&gid=0"
+        "https://docs.google.com/spreadsheets/d/1F_w2yXvD66m0DeSmUrn-mFYcHwr2VKL6JYS6-bdATtQ/edit?gid=0#gid=0"
     ),
     "GD": (
-        "https://docs.google.com/spreadsheets/d/1F_w2yXvD66m0DeSmUrn-mFYcHwr2VKL6JYS6-bdATtQ/export?format=csv&gid=1431418978"
+        "https://docs.google.com/spreadsheets/d/1F_w2yXvD66m0DeSmUrn-mFYcHwr2VKL6JYS6-bdATtQ/edit?gid=1431418978#gid=1431418978"
     ),
     "NCKH": (
-        "https://docs.google.com/spreadsheets/d/10Vb_sP5IEMGkSwCQiUxvN4hrL66RXXtrLLl6e2sG8mg/export?format=csv&gid=0"
+        "https://docs.google.com/spreadsheets/d/10Vb_sP5IEMGkSwCQiUxvN4hrL66RXXtrLLl6e2sG8mg/edit?gid=0#gid=0"
     ),
     "Other": (
-        "https://docs.google.com/spreadsheets/d/1F_w2yXvD66m0DeSmUrn-mFYcHwr2VKL6JYS6-bdATtQ/export?format=csv&gid=1443108898"
+        "https://docs.google.com/spreadsheets/d/1F_w2yXvD66m0DeSmUrn-mFYcHwr2VKL6JYS6-bdATtQ/edit?gid=1443108898#gid=1443108898"
     ),
 }
 # ==========================================================
